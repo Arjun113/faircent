fcf4d9257593aea8c68d13c3506636a4e044957a
Arjun113 <arjunsanghi2005@gmail.com>
2022-06-16 15:23:40 +0530

    Added a second set of datasets provided by M. Sohail containing PAN and Aadhar data. Also started work on the second stage powered by MMOCR

--- Stage1UnnamedAlt.py
+++ Stage1UnnamedAlt.py
@@ -6,23 +6,82 @@ The way to do it is to remove the if condition check for whether the file is nam
 '''
 
 import os
+import json
 from spellchecker import SpellChecker
 from translate import Translator
 from mmocr.utils.ocr import MMOCR
+from PIL import Image
 
-mmocr = MMOCR(det = None, recog = "CRNN_TPS")
+mmocr_config_dir = "C:\\Users\\ArjunSanghi\\Desktop\\Foreign Admission\\Faircent\\Work 1\\mmocr\\configs" # Change this for your system
+mmocr = MMOCR(config_dir= mmocr_config_dir)
 spellcheck = SpellChecker()
 translate = Translator(to_lang='en')
 
-ocrBaseFilesPath = # Indicate the path for the base OCR recognition if needed. Should not be necessary at all
-
 def recognizeFiles(filepath, namedict):
+    panDocumentCounter = 0
+    aadharDocumentCounter = 0
+    chequeDocumentCounter = 0
+    rentAgreementDocumentCounter = 0
+    bankStatementDocumentCounter = 0
     if os.path.exists(filepath) == False:
         return
+    results = mmocr.readtext(filepath, export = filepath)
+
     for document in os.listdir(filepath):
-        splitPath = list(filepath.split("\\"))
-        usedPath = os.path.join(splitPath[len(splitPath-1)], document)
-        namedictKeys = namedict.keys()
+        numberOfEmptyStrings = 0
+        os.mkdir(os.path.join(filepath, "temp"))
+        imageCorrectlyOCRed = False
+        # MMOCR dictates that empty strings will usually be found when the content is not rotated correctly
+        # I will use this property to check for rotation and correct it
+        if "out_" in document:
+            while imageCorrectlyOCRed == False:
+                imageCorrectlyOCRed = True
+                with open(os.path.join(filepath, document), "r") as f:
+                    parsedJSONDict = json.load(f)
+                    usableText = list(parsedJSONDict.get("text"))
+                    for word in usableText:
+                        if word == "":
+                            numberOfEmptyStrings += 1
+                    if numberOfEmptyStrings >= 5: # Change this as per results seen
+                        # Image not rotated correctly
+                        imageCorrectlyOCRed = False
+                        with Image.open(os.path.join(filepath, document[4:len(document) - 4] + ".png")) as img:
+                            img.rotate(90).save(os.path.join(filepath, "temp\\" + document[4:len(document) - 4] + ".png"))
+                        result = mmocr.readtext(os.path.join(filepath, "temp\\" + document[4:len(document) - 4] + ".png"), export = filepath)
+                    else:
+                        imageCorrectlyOCRed = True
+                    f.close()
+
+
+
+    for document in os.listdir(filepath):
+        if "out_" in document:
+            with open(os.path.join(filepath, document), "r") as f:
+                parsedJSONDict = json.load(f)
+                usableText = list(parsedJSONDict.get("text"))
+                # PAN card verification
+                if "permanent" in usableText and "account" in usableText and "number" in usableText:
+                    # Renaming
+                    source = os.path.join(filepath, document[4:len(document) - 4] + ".png")
+                    dest = os.path.join(filepath, "PAN" + str(panDocumentCounter) + ".png")
+                    os.rename(source, dest)
+                    panDocumentCounter += 1
+                # Aadhaar card verification
+                elif "aadhaar" in usableText:
+                    # Renaming
+                    source = os.path.join(filepath, document[4:len(document) - 4] + ".png")
+                    dest = os.path.join(filepath, "AADHAAR" + str(aadharDocumentCounter) + ".png")
+                    os.rename(source, dest)
+                    aadharDocumentCounter += 1
+                # Add others here for chwque and bank statement
+                # else move for manual inspection if criteria does not fit
+
+
+
+
+
+
+
 
 
 
